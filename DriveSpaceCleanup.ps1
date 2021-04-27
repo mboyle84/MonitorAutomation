fefd3056f1392a183a02c4011572bc2b52c3e9c7
Mike B <michael.r.boyle@gmail.com>
2021-04-27 13:57:02 -0400

    drive space clean up script
    can be modified to add more paths, I could have path test but figure this is good enough

---
+++ DriveSpaceCleanup.ps1
@@ -0,0 +1,36 @@
+#written by: Mike B 2021 April 27
+#Contact: 
+#
+#error handling and logging
+#Set-StrictMode -Version latest
+$Error.clear()
+$Error
+$ErrorActionPreference = "Stop"
+$RecycleBinPath1 = $env:systemdrive + '\$Recycle.bin'
+$RecycleBinPath2 = 'D:\$Recycle.bin'
+$TempPath1= “C:\Windows\Temp\*”
+$TempPath2= “C:\Windows\Prefetch\*”
+$TempPath3= “C:\Documents and Settings\*\Local Settings\temp\*”
+$TempPath4= “C:\Windows\Temp\*”
+$TempPath5= “C:\Users\*\Appdata\Local\Temp\*”
+$tempfolders = @($RecycleBinPath1, $RecycleBinPath2, $TempPath1, $TempPath2, $TempPath3, $TempPath4, $TempPath5)
+
+function timestamp() {
+    return ("$(Get-Date) >")
+}
+try
+{
+Write-Host "$(timestamp) [INFO] Beginning of drive cleaning script"
+Write-Host "$(timestamp) [INFO] Attempting to remove RecycleBin with powershell" 
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+Write-Host "$(timestamp) [INFO] Empty various recylcing and temp Folders $tempfolders" 
+Remove-Item $tempfolders -Recurse -Force -ErrorAction SilentlyContinue  
+}
+catch
+{
+    Write-Host "$(timestamp) [ERROR] Encountered processing sessions" -ForegroundColor Red
+    Write-Error $_
+    exit 1
+}
+Write-Host "$(timestamp) [INFO] Ended script successfully"
+
